17362724bc8e5e76825c87bc5154adb9126b2ee8
moumita podder <75267902+moumitapodder@users.noreply.github.com>
2022-09-30 01:56:59 +0530

    Add files via upload
    chatbot using js,html and css

---
+++ response.js
@@ -0,0 +1,36 @@
+const responseObj = {
+    "hello": "Hey cutie ! How are you doing ?",
+    "hey": "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    "Hi":"Hello",
+        "How Are You?":"Great !",
+        "Bye":"Thank you for using me !",
+        "Hello":"Hi , Whats Up",
+        "goodnight": "good night,sweet dream",
+        "I love you": "oops! for how much time",
+        "Talk to you later":"Hope to meet you soon",
+        "male or female" : "sorry , am a robot",
+        "BIRTHDAY": "THE DAY, YOU STARTED USING ME",
+        "DO YOU LOVE ME":"YESSSSS!",
+        "LOVE": "OOPS! FOR NOW , KEEP DISTANCE FROM IT",
+        "good morning" : "good morning dear, have a cup of tea",
+        "not feeling well" : "think about some evenets you wanna recreate in future",
+        "thank you": "welcome",
+        "Do you like people":"yes!",
+        "Do you know Bengali": "No! but you can teach me",
+        "What is your name": "by the name which you will call me",
+        "Who's your master": "you mam/sir",
+        "Can you help me": "sure! i will try",
+        "Feeling lonely": "do code dear, i am alaways with you",
+        "Will you marry me": "I will think",
+        "wanna hug someone": "go,hug your muumma",
+        "single?": "Yes, beacuse of you mam/sir",
+        "sorry": "its ok!,lets change the topic",
+        "ok": "hmmm",
+        "Name": "please ask by:What is your name"
+
+    
+  
+ 
+  };
